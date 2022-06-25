55138383a22f2f0294fcf0d95b29a1310227b77d
Paulius Juodsnukis <paulius.juodsnukis@gmail.com>
2022-06-25 21:56:36 +0300

    Add key and gitignore files
    Add key file and add it to gitignore file

--- js/sript.js
+++ js/sript.js
@@ -1,10 +1,1 @@
-let currentTime = Date.now();
-const debugging = true;
-document.addEventListener("DOMContentLoaded",
-    () => {
-        if (debugging) {
-            console.log("Dom loading took: " + (Date.now() -currentTime ) +'ms');
-        }
-    }
-    
-);
+
